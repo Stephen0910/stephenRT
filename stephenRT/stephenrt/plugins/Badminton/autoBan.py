c8c6406eca96b2672c8d9a28573c87c66c5b9c1e
zhangjian <281016636@qq.com>
2022-03-31 17:37:25 +0800

    同步

--- stephenRT/stephenrt/plugins/Badminton/autoBan.py
+++ stephenRT/stephenrt/plugins/Badminton/autoBan.py
@@ -70,7 +70,7 @@ def filter_chat(chats_list):
                                                                                                         "name"]) + " " + str(
                             chat["sendContent"]).replace("\n", "") + str(chat["isFh"]) + str(chat["isJy"])
                         return result
-        elif re.match("3564837153|166345259|3569544846|2927295662|万钻|万钴|万砖|万鉆", str(chat["sendMan"]["name"])):
+        elif re.match("3564837153|2580237802|166345259|3569544846|2927295662|万钻|万钴|万砖|万鉆|萬鉆|萬钻", str(chat["sendMan"]["name"])):
             result = "chatRoom疑似广告：" + str(chat["sendMan"]["numberUserId"]) + " " + str(chat["sendMan"][
                                                                                             "name"]) + " " + str(
                 chat["sendContent"]).replace("\n", "") + str(chat["isFh"]) + str(chat["isJy"])
