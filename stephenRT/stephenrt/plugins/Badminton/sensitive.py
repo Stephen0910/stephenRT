9265191ac4251648f9cfe589c01c47db263d9f1e
zhangjian <281016636@qq.com>
2022-04-06 11:55:05 +0800

    只剩开关

--- stephenRT/stephenrt/plugins/Badminton/sensitive.py
+++ stephenRT/stephenrt/plugins/Badminton/sensitive.py
@@ -41,13 +41,14 @@ def get_host_ip():
 
 
 if re.search("192", get_host_ip()):
-    group_id = config["group_id_test"]
-    check_gpName = "决战羽毛球"
-else:
-    group_id = config["group_id_test"]
+    is_delete = True
     check_gpName = "Robot"
+else:
+    is_delete = False
+    check_gpName = "决战羽毛球"
 
-is_delete = True
+group_id = config["group_id_test"]
+
 
 print(group_id, check_gpName)
 
@@ -100,8 +101,8 @@ async def send_private(bot: Bot, user_id, msg):
     await bot.send_private_msg(user_id=user_id, message=str(msg))
 
 
-async def ban_user(bot: Bot, group_id, user_id, duration):
-    await bot.set_group_ban(group_id, user_id, duration * 60)
+async def ban_user(bot: Bot, group_id, user_id, time):
+    await bot.set_group_ban(group_id=group_id, user_id=user_id, duration=time * 60)
 
 
 async def delete_msg(bot: Bot, msgid):
@@ -119,11 +120,19 @@ async def delete_msg(bot: Bot, msgid):
         await send_private(bot, user_id=report_to, msg=result)
 
 
-async def exe_dirty(sql):
+async def save_dirty(sql):
     conn = await asyncpg.connect(user=config["user"], password=config["password"], database=config["database"],
                                  host=config["host"])
     await conn.execute(sql)
     await conn.close()
+
+
+async def select_dirty(sql):
+    conn = await asyncpg.connect(user=config["user"], password=config["password"], database=config["database"],
+                                 host=config["host"])
+    user_count = await conn.fetchrow(sql)
+    await conn.close()
+    return user_count["count"]
 
 
 msg_matcher = on_message()
@@ -141,12 +150,9 @@ async def checkMessage(bot: Bot, event: GroupMessageEvent):
     jieba_msg = re.sub('\[CQ:\w+,.+?\]', "", or_msg)  # 图片等信息过滤
     sen_text = re.compile(u'[\u4E00-\u9FA5|\s\w]').findall(jieba_msg)  # 去掉所有标点符号
     pure_msg = "".join(sen_text)
-    print("pure_msg:", pure_msg)
     jieba.load_userdict(sens)  # 优先拆分敏感词
     jieba.load_userdict(get_white())  # 白名单词
     content = jieba.lcut(pure_msg, cut_all=False)  # 避免误报，使用分词
-    print("分词结果:", content)
-
     for word in sens:  # word: 屏蔽词库  content: message的结巴分词列表
         if word in content:
             print("word:", word, len(word))
@@ -161,13 +167,12 @@ async def checkMessage(bot: Bot, event: GroupMessageEvent):
             else:
                 name = sender.nickname
             if check_gpName in group_name:  # 这里要做权限隔离  不要所有都检测
-                print("符合群规则")
                 dateArray = datetime.datetime.utcfromtimestamp(msg.time + 8 * 3600)  # 时区加8)
                 msg_time = dateArray.strftime("%Y-%m-%d %H:%M:%S")
                 sql = """
                 INSERT INTO "public"."dirty"("timestamp", "group_id", "group_name", "user_id", "user_name", "message", "key") VALUES ('{0}', {1}, '{2}', {3}, '{4}', '{5}', '{6}');
                 """.format(msg_time, group_id, group_name, sender_id, name, or_msg, word)
-                await exe_dirty(sql)
+                await save_dirty(sql)
                 send_message = "{0}|{1} 发送敏感内容:【{2}】\n(敏感词:{3})".format(group_name, name, or_msg, word)
                 # await send_private(bot, user_id=report_to,
                 #                    msg="{0}|{1} 发送敏感内容:【{2}】\n(敏感词:{3})".format(group_name, name, or_msg, word))
@@ -179,15 +184,15 @@ async def checkMessage(bot: Bot, event: GroupMessageEvent):
                 finally:
                     user_sql = """
                     SELECT count(id) FROM "dirty" WHERE user_id = {0} and group_id = {1}
-                    """.format(user_id, group_id)
-                    count = await exe_dirty(user_sql)
+                    """.format(sender_id, group_id)
+                    count = await select_dirty(user_sql)
                     print("共有%s条数据" % count)
                     if is_delete == True:
-                        await delete_msg(bot, message_id) # 暂不启用
-                        if count > 2:
-                            time = (count / 3) * 5
-                            if count % 3 == 0:
-                                await ban_user(group_id, user_id, time)
+                        await delete_msg(bot, message_id)  # 暂不启用
+                        if count > 2 and count % 3 == 0:
+                            time = (count // 3) * 5
+                            print("time:", time)
+                            await bot.set_group_ban(group_id=group_id, user_id=sender_id, duration=time*60)
                 break  # 重复的脏字会导致发送两次修复
 
     # for word in content:
