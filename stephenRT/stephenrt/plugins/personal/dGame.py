c94227a9b8a6475ed419dd5e093ea143d68b4fce
zhangjian <281016636@qq.com>
2022-04-12 16:44:44 +0800

    移除首行换行

--- stephenRT/stephenrt/plugins/personal/dGame.py
+++ stephenRT/stephenrt/plugins/personal/dGame.py
@@ -16,6 +16,7 @@ from nonebot import get_bot
 import re
 import asyncio
 import socket
+from nonebot.adapters.onebot.v11.message import MessageSegment
 
 
 def get_host_ip():
@@ -102,7 +103,7 @@ async def game_info():
         g_id = data["g_id"]
         g_source = data["g_source"]
         t_create_time = int(time.mktime(time.strptime(create_time, "%Y-%m-%dT%H:%M:%S")))
-        print(t_create_time)
+        # print(t_create_time)
         if t_create_time > time_list[-1]:
             g_ids.append(g_id)
             time_list.append(t_create_time)
@@ -115,6 +116,10 @@ async def game_info():
         is_win = "OMG 胜" if data["game_result"] == "0" else "OMG 负"
         id_url = "https://score.09game.com/MOBA/CorrelationPlayerMilitaryExploit?GameTypeID=21&GameID={0}&GameSource={1}&CurrentSeason=0".format(
             new_id, g_source)
+        # ima_url = "https://www.09game.com/html/2020gamescore/web/gamedetail/21.html?sessid=0&gameid={0}".format(
+        #     new_id)
+        # print("ima_url:", ima_url)
+        # image = MessageSegment.image(file=ima_url)
         omg_spend = int(data["time_length"]) // 60 + 1
         detail = json.loads(requests.get(id_url).content)
         print("执行omg具体信息")
@@ -123,15 +128,20 @@ async def game_info():
             if data["user_name"] in ids.keys():
                 kda = "{0}/{1}/{2}".format(data["kill_count"], data["killed_count"], data["assist_count"])
                 hero_name, hero_level = data["hero_name"], data["hero_level"]
-                omg_msg = is_win + " {0}分钟\n".format(omg_spend) + name + "-" + hero_name + ":" + kda
+                omg_msg = is_win + " {0}分钟\n".format(omg_spend) + name + "-" + hero_name + ":" + kda + "\n"
                 # print(omg_msg)
-                o_msg = "\n" + omg_msg
+                o_msg += omg_msg
         print(o_msg)
         if len(o_msg) > 5:
             try:
                 await bot.send_group_msg(group_id=group, message=o_msg)
             except Exception as e:
                 await bot.send_private_msg(user_id=281016636, message=str(o_msg) + str(e))
+
+            # try:
+            #     await bot.send_group_msg(group_id=group, message=image)
+            # except Exception as e:
+            #     await bot.send_private_msg(user_id=281016636, message=image)
 
     else:
         print("OMG无")
@@ -159,21 +169,29 @@ async def game_info():
         is_win = "龙魂 胜" if dg_data["game_result"] == "0" else "龙魂 负"
         id_url = "https://score.09game.com/RPG/GamePerformanceListJson?GameTypeID=142&gameid={0}&gamesource=".format(
             new_id)
+        # ima_url = "https://www.09game.com/html/2020gamescore/web/gamedetail/142.html?userid={0}&gameid={1}".format(id,
+        #                                                                                                    new_id)
+        # image = MessageSegment.image(file=ima_url)
         dg_spend = int(dg_data["time_length"]) // 60 + 1
         dg_detail = json.loads(requests.get(id_url).content)
         print("执行dg具体信息")
         for data in dg_detail["data"]:
             if data["user_name"] in ids.keys():
                 kda = re.match("击杀:\d+;死亡:\d+;助攻:\d+;", data["extra_value"]).group()
-                dg_msg = is_win + " {0}分钟\n".format(dg_spend) + data["user_name"] + ":" + kda
+                dg_msg = is_win + " {0}分钟\n".format(dg_spend) + data["user_name"] + ":" + kda + "\n"
                 print(dg_msg)
-                d_msg = "\n" + dg_msg
+                d_msg += dg_msg
 
         if len(d_msg) > 5:
             try:
                 await bot.send_group_msg(group_id=group, message=d_msg)
             except Exception as e:
                 await bot.send_private_msg(user_id=281016636, message=str(d_msg) + str(e))
+
+            # try:
+            #     await bot.send_group_msg(group_id=group, message=image)
+            # except Exception as e:
+            #     await bot.send_private_msg(user_id=281016636, message=image)
 
 
 # print(get_recent_data(369818))
