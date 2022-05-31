41a3824dd60966cdc93f8d4beb13f7ffd5a89a67
zhangjian <281016636@qq.com>
2022-05-31 11:03:09 +0800

    dgametrigger2

--- stephenRT/stephenrt/plugins/personal/dGame.py
+++ stephenRT/stephenrt/plugins/personal/dGame.py
@@ -25,7 +25,7 @@ import urllib3
 urllib3.disable_warnings()
 requests.adapters.DEFAULT_RETRIES = 5
 
-sleep_time = 7
+
 v_url = "https://api.linhun.vip/api/Littlesistervideo?type=json"
 
 # names = ["Dream丶狗", "a824683653"]
@@ -168,7 +168,6 @@ def get_ids(names):
                 ids[name] = id
                 response.close()
         except:
-            time.sleep(1)
             print("连接错误 重试")
         print(ids)
     return ids
@@ -225,176 +224,182 @@ else:
 
 print("first_time:", first_time)
 time_list = [first_time]
-
+trigger = 1
 
 @matcher.handle()
 async def game_info():
-    bot = get_bot()
-    g_ids = []
-    o_msg = ""
-    d_msg = ""
-    for name, id in ids.items():
-        try:
-            data = await get_recent_data(id)
-        except:
-            data = ""
-        create_time = data["create_time"]
-        g_id = data["g_id"]
-        g_source = data["g_source"]
-        g_type = game_source[g_source]
-        t_create_time = int(time.mktime(time.strptime(create_time, "%Y-%m-%dT%H:%M:%S")))
-        # print(t_create_time)
-        if t_create_time > time_list[-1]:
-            g_ids.append(g_id)
-            time_list.append(t_create_time)
-            break
-        # else:
-        #     print(t_create_time)
-
-    if len(g_ids) > 0:
-        new_id = g_ids[0]
-        is_win = "OMG 胜" if data["game_result"] == "0" else "OMG 负"
-        id_url = "https://score.09game.com/MOBA/CorrelationPlayerMilitaryExploit?GameTypeID=21&GameID={0}&GameSource={1}&CurrentSeason=0".format(
-            new_id, g_source)
-        source_url = "https://cdn.09game.com/resources/game_skill/"
-        omg_spend = int(data["time_length"]) // 60 + 1
-        response = requests.get(id_url, verify=False, timeout=3)
-        detail = json.loads(response.content)
-        response.close()
-        # print(json.dumps(detail))
-        users = 0
-        for data in detail["data"]:
-            if data["user_name"] in ids.keys():
-                user_id = data["user_id"]
-                kda = "{0}/{1}/{2}".format(data["kill_count"], data["killed_count"], data["assist_count"])
-                hero_name, hero_level = data["hero_name"], data["hero_level"]
-                guard = "近卫" if str(data["team_id"]) == "0" else "天灾"
-                c_skills = [transfer_dId(x) for x in data["skills"].split(",")][4:6]
-                title = data["title"]
-                user_title = " " + get_title(title) + " "
-                # print("c_skills:", c_skills)
-                skill1 = MessageSegment.image(source_url + c_skills[0] + ".jpg")
-                skill2 = MessageSegment.image(source_url + c_skills[1] + ".jpg")
-                o_msg = o_msg + data[
-                    "user_name"] + "-" + hero_name + "-" + str(
-                    hero_level) + "级 " + guard + ":" + kda + user_title + "\n" + skill1 + skill2 + "\n"
-                # 暂不显示skills, guard-天灾近卫
-                # o_msg = o_msg + data[
-                #     "user_name"] + "-" + hero_name + "-" + str(
-                #     hero_level) + "级 " + ":" + kda + user_title + "\n"
-                # print(omg_msg)
-                users += 1
-                team_id = data["team_id"]
-                mc_gids = await get_gids(user_id)
-
-        for data in detail["data"]:
-            if data["user_name"] not in ids:
-                if data["team_id"] == team_id and data["user_name"] not in ids:
-                    street_ids = await get_gids(data["user_id"])
-                    same_ids = list(set(mc_gids) & set(street_ids))
-                    if len(same_ids) > 3:
-                        users += 1
-                        print(data["user_name"])
-
-                if g_type == "赛季挨打-" and users >=3:
-                    users = 3
-                people = "{0}排 ".format(users_chi[users])
-
-        # 上面是人数
-        omg_msg = "报：" + g_type + people + is_win + " {0}分钟\n".format(omg_spend) + o_msg
-
-        pic = "战绩图： https://www.09game.com/html/2020gamescore/web/gamedetail/21.html?sessid=0&gameid={0}".format(new_id)
-        # pic = '<a href="{0}">超链接</a>'.format(pic)
-        print("pic:", pic)
-        omg_msg = omg_msg + pic
-        print(omg_msg, len(omg_msg))
-
-        # if len(omg_msg) > 1:
-        if len(omg_msg) > 1:
-            print("send the omg msg")
+    global trigger
+    if trigger % 2 == 0:
+        bot = get_bot()
+        g_ids = []
+        o_msg = ""
+        d_msg = ""
+        for name, id in ids.items():
             try:
-                await bot.send_group_msg(group_id=group, message=omg_msg)
-            except Exception as e:
-                await bot.send_private_msg(user_id=281016636, message=str(omg_msg) + str(e))
+                data = await get_recent_data(id)
+            except:
+                data = ""
+            create_time = data["create_time"]
+            g_id = data["g_id"]
+            g_source = data["g_source"]
+            g_type = game_source[g_source]
+            t_create_time = int(time.mktime(time.strptime(create_time, "%Y-%m-%dT%H:%M:%S")))
+            # print(t_create_time)
+            if t_create_time > time_list[-1]:
+                g_ids.append(g_id)
+                time_list.append(t_create_time)
+                break
+            # else:
+            #     print(t_create_time)
 
-        if is_win == "OMG 胜" and ip == "10.10.10.8":
-            send = 0
-            while send == 0:
-                video_url = await get_video()
-                pic_file = MessageSegment.video(file=video_url)
+        if len(g_ids) > 0:
+            new_id = g_ids[0]
+            is_win = "OMG 胜" if data["game_result"] == "0" else "OMG 负"
+            id_url = "https://score.09game.com/MOBA/CorrelationPlayerMilitaryExploit?GameTypeID=21&GameID={0}&GameSource={1}&CurrentSeason=0".format(
+                new_id, g_source)
+            source_url = "https://cdn.09game.com/resources/game_skill/"
+            omg_spend = int(data["time_length"]) // 60 + 1
+            response = requests.get(id_url, verify=False, timeout=3)
+            detail = json.loads(response.content)
+            response.close()
+            # print(json.dumps(detail))
+            users = 0
+            for data in detail["data"]:
+                if data["user_name"] in ids.keys():
+                    user_id = data["user_id"]
+                    kda = "{0}/{1}/{2}".format(data["kill_count"], data["killed_count"], data["assist_count"])
+                    hero_name, hero_level = data["hero_name"], data["hero_level"]
+                    guard = "近卫" if str(data["team_id"]) == "0" else "天灾"
+                    c_skills = [transfer_dId(x) for x in data["skills"].split(",")][4:6]
+                    title = data["title"]
+                    user_title = " " + get_title(title) + " "
+                    # print("c_skills:", c_skills)
+                    skill1 = MessageSegment.image(source_url + c_skills[0] + ".jpg")
+                    skill2 = MessageSegment.image(source_url + c_skills[1] + ".jpg")
+                    o_msg = o_msg + data[
+                        "user_name"] + "-" + hero_name + "-" + str(
+                        hero_level) + "级 " + guard + ":" + kda + user_title + "\n" + skill1 + skill2 + "\n"
+                    # 暂不显示skills, guard-天灾近卫
+                    # o_msg = o_msg + data[
+                    #     "user_name"] + "-" + hero_name + "-" + str(
+                    #     hero_level) + "级 " + ":" + kda + user_title + "\n"
+                    # print(omg_msg)
+                    users += 1
+                    team_id = data["team_id"]
+                    mc_gids = await get_gids(user_id)
+
+            for data in detail["data"]:
+                if data["user_name"] not in ids:
+                    if data["team_id"] == team_id and data["user_name"] not in ids:
+                        street_ids = await get_gids(data["user_id"])
+                        same_ids = list(set(mc_gids) & set(street_ids))
+                        if len(same_ids) > 3:
+                            users += 1
+                            print(data["user_name"])
+
+                    if g_type == "赛季挨打-" and users >=3:
+                        users = 3
+                    people = "{0}排 ".format(users_chi[users])
+
+            # 上面是人数
+            omg_msg = "报：" + g_type + people + is_win + " {0}分钟\n".format(omg_spend) + o_msg
+
+            pic = "战绩图： https://www.09game.com/html/2020gamescore/web/gamedetail/21.html?sessid=0&gameid={0}".format(new_id)
+            # pic = '<a href="{0}">超链接</a>'.format(pic)
+            print("pic:", pic)
+            omg_msg = omg_msg + pic
+            print(omg_msg, len(omg_msg))
+
+            # if len(omg_msg) > 1:
+            if len(omg_msg) > 1:
+                print("send the omg msg")
                 try:
-                    await bot.send_group_msg(group_id=group, message=pic_file)
-                    send = 1
-                except:
-                    print("发送失败，重试")
+                    await bot.send_group_msg(group_id=group, message=omg_msg)
+                except Exception as e:
+                    await bot.send_private_msg(user_id=281016636, message=str(omg_msg) + str(e))
 
-        elif is_win == "OMG 负" and ip == "10.10.10.8":
-            award_url = await get_rPic()
-            image = MessageSegment.image(award_url)
+            if is_win == "OMG 胜" and ip == "10.10.10.8":
+                send = 0
+                while send == 0:
+                    video_url = await get_video()
+                    pic_file = MessageSegment.video(file=video_url)
+                    try:
+                        await bot.send_group_msg(group_id=group, message=pic_file)
+                        send = 1
+                    except:
+                        print("发送失败，重试")
+
+            elif is_win == "OMG 负" and ip == "10.10.10.8":
+                award_url = await get_rPic()
+                image = MessageSegment.image(award_url)
+                try:
+                    await bot.send_group_msg(group_id=group, message=image)
+                except Exception as e:
+                    await bot.send_private_msg(user_id=281016636, message=str(e))
+
+        # else:
+        #     print("OMG无")
+
+        # DG模式
+        dg_ids = []
+        for name, id in ids.items():
             try:
-                await bot.send_group_msg(group_id=group, message=image)
-            except Exception as e:
-                await bot.send_private_msg(user_id=281016636, message=str(e))
+                dg_data = await get_dg_id(id)
+            except:
+                dg_data = "无"  # 没有对局
+                continue
+            dg_create_time = dg_data["create_time"]
+            dg_id = dg_data["game_id"]
+            dg_create_time = int(time.mktime(time.strptime(dg_create_time, "%Y-%m-%dT%H:%M:%S")))
 
-    # else:
-    #     print("OMG无")
+            # print(dg_create_time)
+            if dg_create_time > time_list[-1]:
+                dg_ids.append(dg_id)
+                time_list.append(dg_create_time)
+                break
 
-    # DG模式
-    dg_ids = []
-    for name, id in ids.items():
-        try:
-            dg_data = await get_dg_id(id)
-        except:
-            dg_data = "无"  # 没有对局
-            continue
-        dg_create_time = dg_data["create_time"]
-        dg_id = dg_data["game_id"]
-        dg_create_time = int(time.mktime(time.strptime(dg_create_time, "%Y-%m-%dT%H:%M:%S")))
+        if len(dg_ids) > 0:
+            new_id = dg_ids[0]
+            is_win = "龙魂 胜" if dg_data["game_result"] == "0" else "龙魂 负"
+            id_url = "https://score.09game.com/RPG/GamePerformanceListJson?GameTypeID=142&gameid={0}&gamesource=".format(
+                new_id)
+            dg_spend = int(dg_data["time_length"]) // 60 + 1
+            response = requests.get(id_url, verify=False, timeout=3)
+            dg_detail = json.loads(response.content)
+            response.close()
+            for data in dg_detail["data"]:
+                if data["user_name"] in ids.keys():
+                    # 图片
+                    user_title = ""
+                    extra_value = str(data["extra_value"])
+                    hero = re.search("英雄:.*?;", extra_value).group()[-5:-1]
+                    skill1, skill2 = re.search("额外技能1:.*?;", extra_value).group()[-5:-1], re.search("额外技能2:.*?;",
+                                                                                                    extra_value).group()[
+                                                                                          -5:-1]
+                    hero_icon = MessageSegment.image("https://cdn.09game.com/resources/game_avator/" + hero + ".jpg")
+                    skill1_icon = MessageSegment.image("https://cdn.09game.com/resources/game_skill/" + skill1 + ".jpg")
+                    skill2_icon = MessageSegment.image("https://cdn.09game.com/resources/game_skill/" + skill2 + ".jpg")
 
-        # print(dg_create_time)
-        if dg_create_time > time_list[-1]:
-            dg_ids.append(dg_id)
-            time_list.append(dg_create_time)
-            break
+                    kda = re.match("击杀:\d+;死亡:\d+;助攻:\d+;", data["extra_value"]).group()
+                    for title in dg_titles.keys():
+                        if data[title] != 0:
+                            user_title += dg_titles[title]
 
-    if len(dg_ids) > 0:
-        new_id = dg_ids[0]
-        is_win = "龙魂 胜" if dg_data["game_result"] == "0" else "龙魂 负"
-        id_url = "https://score.09game.com/RPG/GamePerformanceListJson?GameTypeID=142&gameid={0}&gamesource=".format(
-            new_id)
-        dg_spend = int(dg_data["time_length"]) // 60 + 1
-        response = requests.get(id_url, verify=False, timeout=3)
-        dg_detail = json.loads(response.content)
-        response.close()
-        for data in dg_detail["data"]:
-            if data["user_name"] in ids.keys():
-                # 图片
-                user_title = ""
-                extra_value = str(data["extra_value"])
-                hero = re.search("英雄:.*?;", extra_value).group()[-5:-1]
-                skill1, skill2 = re.search("额外技能1:.*?;", extra_value).group()[-5:-1], re.search("额外技能2:.*?;",
-                                                                                                extra_value).group()[
-                                                                                      -5:-1]
-                hero_icon = MessageSegment.image("https://cdn.09game.com/resources/game_avator/" + hero + ".jpg")
-                skill1_icon = MessageSegment.image("https://cdn.09game.com/resources/game_skill/" + skill1 + ".jpg")
-                skill2_icon = MessageSegment.image("https://cdn.09game.com/resources/game_skill/" + skill2 + ".jpg")
+                    d_msg = d_msg + data["user_name"] + ":" + kda + " {} ".format(
+                        user_title) + "\n" + hero_icon + skill1_icon + skill2_icon + "\n"
 
-                kda = re.match("击杀:\d+;死亡:\d+;助攻:\d+;", data["extra_value"]).group()
-                for title in dg_titles.keys():
-                    if data[title] != 0:
-                        user_title += dg_titles[title]
+            dg_msg = "报：" + is_win + " {0}分钟\n".format(dg_spend) + d_msg
+            print(dg_msg)
+            if len(dg_msg) > 1 and ip == "10.10.10.8":
+                print("send dg new msg")
+                try:
+                    await bot.send_group_msg(group_id=group, message=dg_msg)
+                except Exception as e:
+                    await bot.send_private_msg(user_id=281016636, message=str(dg_msg) + str(e))
 
-                d_msg = d_msg + data["user_name"] + ":" + kda + " {} ".format(
-                    user_title) + "\n" + hero_icon + skill1_icon + skill2_icon + "\n"
+    # print(get_recent_data(369818))
+    # print(get_dg_id(369818))
+    else:
+        print("dg trigger pass:{0}".format(trigger))
 
-        dg_msg = "报：" + is_win + " {0}分钟\n".format(dg_spend) + d_msg
-        print(dg_msg)
-        if len(dg_msg) > 1 and ip == "10.10.10.8":
-            print("send dg new msg")
-            try:
-                await bot.send_group_msg(group_id=group, message=dg_msg)
-            except Exception as e:
-                await bot.send_private_msg(user_id=281016636, message=str(dg_msg) + str(e))
-
-# print(get_recent_data(369818))
-# print(get_dg_id(369818))
+    trigger += 1
