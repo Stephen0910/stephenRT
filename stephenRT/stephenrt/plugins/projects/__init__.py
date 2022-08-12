a3a0aad0e128b5596f974655aeec00efecf26992
zhangjian <281016636@qq.com>
2022-08-12 11:11:28 +0800

    1

--- stephenRT/stephenrt/plugins/projects/__init__.py
+++ stephenRT/stephenrt/plugins/projects/__init__.py
@@ -8,5 +8,45 @@
 # @Software : PyCharm
 # @Copyright:   (c) StephenZ 2022
 # @Licence  :     <@2022>
-from .query_game import *
+
+
+import socket
+import re
+
+def get_host_ip():
+    """
+    查询本机ip地址
+    :return: ip
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+ip = str(get_host_ip())
+
+
+
+if re.match("192.*", ip):
+    # from .dGame import *
+    # from .dm_pro.live import *
+    # from .search_body import *
+    # from .nba.season import *
+    # from .kuake import *
+    # from .wPublic import *
+    from .query_game import *
+    pass
+
+if ip == "10.10.10.8":
+    pass
+
+
+if ip == "172.24.121.72":
+
+    pass
+
 
